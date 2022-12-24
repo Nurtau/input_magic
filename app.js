11142d803e8a7ecdc36d205da4f46c5548898830
Nurtau <nurtau.toganbay@targetai.kz>
2022-12-24 23:25:26 +0600

    feat/ add basic list virtualisation

--- app.js
+++ app.js
@@ -3,29 +3,87 @@ const slots = document.querySelector(".slots");
 
 input.addEventListener("input", addItems);
 
+let renderer;
+
 function addItems(event) {
-    event.preventDefault();
-    let number = parseInt(input.value);
-    if (!(isNaN(number))) {
-        while (slots.firstChild) {
-            slots.removeChild(slots.firstChild);
-        };
-        for (let i = 1; i <= number; i++) {
-            let html = `
-            <div class="item">
-                <h1>${i}</h1>
-            </div>`;
-            slots.insertAdjacentHTML("beforeend", html);
-        }
-    } else if (input.value === "") {
-        while (slots.firstChild) {
-            slots.removeChild(slots.firstChild);
-        }
-        let html = `
+  event.preventDefault();
+
+  renderer?.destroy();
+  let number = parseInt(input.value);
+
+  if (!isNaN(number)) {
+    renderer = new VirtualListRenderer(90, slots, number, 4);
+    renderer.setup();
+  } else if (input.value === "") {
+    slots.innerHTML = "";
+
+    let html = `
             <div class="item">
                 <h1>It's me again</h1>
             </div>`;
-        slots.insertAdjacentHTML("beforeend", html);
-    };
+    slots.insertAdjacentHTML("beforeend", html);
+  }
+}
 
-};
+class VirtualListRenderer {
+  constructor(itemHeight, root, maxNumber, overscan) {
+    this.itemHeight = itemHeight;
+    this.root = root;
+    this.maxNumber = maxNumber;
+    this.overscan = overscan;
+  }
+
+  onWindowScroll = () => {
+    const { top } = this.root.getBoundingClientRect();
+    const rootVisibileHeight = window.innerHeight - Math.max(0, top);
+
+    let visibleStartNumber = top < 0 ? Math.floor(-top / this.itemHeight) : 0;
+    let visibleNumberLength = Math.ceil(rootVisibileHeight / this.itemHeight);
+
+    const visibleEndNumber = Math.min(
+      this.maxNumber,
+      visibleStartNumber + visibleNumberLength + this.overscan
+    );
+    visibleStartNumber = Math.max(0, visibleStartNumber - this.overscan);
+
+    const offsetTop = visibleStartNumber * this.itemHeight;
+    const offsetBottom = (this.maxNumber - visibleEndNumber) * this.itemHeight;
+
+    this.root.innerHTML = "";
+
+    this.insertDiv({ height: offsetTop });
+
+    for (let i = visibleStartNumber; i <= visibleEndNumber; i++) {
+      this.insertDiv({ className: "item", content: `<h1>${i}</h1>` });
+    }
+
+    this.insertDiv({ height: offsetBottom });
+  };
+
+  insertDiv({ className, content, height }) {
+    const div = document.createElement("div");
+
+    if (content) {
+      div.innerHTML = content;
+    }
+
+    if (className) {
+      div.classList.add(className);
+    }
+
+    if (height) {
+      div.style.height = `${height}px`;
+    }
+
+    this.root.appendChild(div);
+  }
+
+  setup() {
+    this.onWindowScroll();
+    window.addEventListener("scroll", this.onWindowScroll);
+  }
+
+  destroy() {
+    window.removeEventListener("scroll", this.onWindowScroll);
+  }
+}
